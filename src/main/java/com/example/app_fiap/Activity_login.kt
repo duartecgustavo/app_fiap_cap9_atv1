45c71ce5a636d40ec8da9714faa305d2c844f4c7
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2023-03-04 23:34:51 -0300

    Add files via upload

---
+++ src/main/java/com/example/app_fiap/Activity_login.kt
@@ -0,0 +1,12 @@
+package com.example.app_fiap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ src/main/java/com/example/app_fiap/Activity_main.kt
@@ -0,0 +1,12 @@
+package com.example.app_fiap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class Activity_main : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
